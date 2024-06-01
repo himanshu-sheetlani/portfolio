74bc7354793b2103548510a3dd812ddc12dfff7d
Himanshu Sheetlani <himanshu.sheetlani2909@gmail.com>
2024-06-01 19:43:37 +0530

    making responsive

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ function modeChange(){
     let resume= document.querySelectorAll(".resume");
     let text= document.querySelectorAll(".color");
     let bottom= document.querySelector("#bottom");
+    let about= document.querySelector("#about")
+    let sectionText= document.querySelector(".sectionText")
+    let heading= document.querySelector(".heading")
 
     if (slider.checked) {
         body.style.backgroundColor="var(--blackBg)";
@@ -19,12 +22,17 @@ function modeChange(){
         shape.style.backgroundColor="var(--orange)"
         nav.style.backgroundColor="var(--blackBg)"
         bottom.style.backgroundColor="var(--orange)"
+        about.style.backgroundColor="var(--lightOrange)"
+        sectionText.style.color="var(--white)"
+        heading.style.color="var(--orange)"
         for (let index=0 ; index < resume.length; index++) {
             resume[index].style.backgroundColor="var(--orange)"
         }
         for (let index=0 ; index < text.length; index++) {
             text[index].style.color="var(--white)"
         }
+
+
     }else {
         body.style.backgroundColor="var(--white)";
         name.style.color="var(--black)"
@@ -33,6 +41,9 @@ function modeChange(){
         shape.style.backgroundColor="var(--blue)"
         nav.style.backgroundColor="var(--white)"
         bottom.style.backgroundColor="var(--blue)"
+        about.style.backgroundColor="var(--lightBlue)"
+        sectionText.style.color="var(--back)"
+        heading.style.color="var(--blue)"
         for (let index=0 ; index < resume.length; index++) {
             resume[index].style.backgroundColor="var(--blue)"
         }
