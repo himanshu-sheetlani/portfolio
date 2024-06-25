bd85c99097e1f08659e75b795df505be78698261
Himanshu Sheetlani <himanshu.sheetlani2909@gmail.com>
2024-06-25 17:51:01 +0530

    optimised dark mode

--- app.js
+++ app.js
@@ -3,110 +3,21 @@ console.log("hello")
 // alert("this portfolio is still under construction but still you can visit it...       ~Himanshu Sheetlani")
 
 function modeChange(){
-    var body = document.body;
     let slider = document.querySelector("#tickBox");
-    let name= document.querySelector("#name"); 
-    let hello = document.querySelector("#hello");
-    let designation= document.querySelector("#designation");
-    let shape= document.querySelector(".shape");
-    let nav= document.querySelector("#nav");
-    let resume= document.querySelectorAll(".resume");
-    let text= document.querySelectorAll(".color");
-    let bottom= document.querySelector("#bottom");
-    let about= document.querySelector("#about")
-    let sectionText= document.querySelector(".sectionText")
-    let heading= document.querySelector(".heading")
-    let c1= document.querySelector("#c1")
-    let c2= document.querySelector("#c2")
-    let highlight= document.querySelectorAll(".highlight")
-    let popover= document.querySelectorAll(".popover")
-    let popoverBefore=  document.querySelectorAll(".popover::before")
-    let certificate= document.querySelectorAll(".flip-front")
-    let certiBack= document.querySelectorAll(".flip-back")
-    let low = document.querySelector("#low")
-    let seeAll = document.querySelector(".button")
-
+    
     if (slider.checked) {
-        body.style.backgroundColor="var(--blackBg)";
-        name.style.color="var(--white)"
-        hello.style.color="var(--white)"
-        designation.style.color="var(--orange)"
-        shape.style.backgroundColor="var(--orange)"
-        nav.style.backgroundColor="var(--blackBg)"
-        bottom.style.backgroundColor="var(--orange)"
-        about.style.backgroundColor="var(--lightOrange)"
-        sectionText.style.color="var(--white)"
-        heading.style.color="var(--orange)"
-        c1.style.backgroundColor="var(--orange)"
-        c2.style.backgroundColor="var(--lightOrange)"
-        low.style.backgroundColor="var(--orange)"
-        seeAll.style.color="var(--orange)"
-        seeAll.style.textShadow="0 0 15px var(--orange)";
-        // highlight.style.background="var(--orange)"
-        for (let index=0 ; index < resume.length; index++) {
-            resume[index].style.backgroundColor="var(--orange)"
-        }
-        for (let index=0 ; index < text.length; index++) {
-            text[index].style.color="var(--white)"
-        }
-        for (let index=0 ; index < highlight.length; index++) {
-            highlight[index].style.color="var(--orange)"
-        }
-        for (let index=0 ; index < popover.length; index++) {
-            popover[index].style.backgroundColor="var(--orange)"
-        }
-        for (let index=0 ; index < popoverBefore.length; index++) {
-            popoverBefore[index].style.backgroundColor="var(--orange)"
-        }
-        for (let index=0 ; index < certificate.length; index++) {
-            certificate[index].style.backgroundColor="var(--orange)"
-        }
-        for (let index=0 ; index < certiBack.length; index++) {
-            certiBack[index].style.backgroundColor="var(--orange)"
-        }
-
-
-    }else {
-        body.style.backgroundColor="var(--white)";
-        name.style.color="var(--black)"
-        hello.style.color="var(--black)"
-        designation.style.color="var(--blue)"
-        shape.style.backgroundColor="var(--blue)"
-        nav.style.backgroundColor="var(--white)"
-        bottom.style.backgroundColor="var(--blue)"
-        about.style.backgroundColor="var(--lightBlue)"
-        sectionText.style.color="var(--back)"
-        heading.style.color="var(--blue)"
-        c1.style.backgroundColor="var(--blue)"
-        c2.style.backgroundColor="var(--lightBlue)"
-        low.style.backgroundColor="var(--blue)"
-        seeAll.style.color="var(--blue)";
-        seeAll.style.textShadow="0 0 15px var(--blue)";
-
-        for (let index=0 ; index < resume.length; index++) {
-            resume[index].style.backgroundColor="var(--blue)"
-        }
-        for (let index=0 ; index < text.length; index++) {
-            text[index].style.color="var(--black)"
-        }
-        for (let index=0 ; index < highlight.length; index++) {
-            highlight[index].style.color="var(--blue)"
-        }
-        for (let index=0 ; index < popover.length; index++) {
-            popover[index].style.backgroundColor="var(--blue)"
-        }
-        for (let index=0 ; index < certificate.length; index++) {
-            certificate[index].style.backgroundColor="var(--blue)"
-        }
-        for (let index=0 ; index < certiBack.length; index++) {
-            certiBack[index].style.backgroundColor="var(--blue)"
-        }
-        
+        document.querySelector(":root").style.setProperty('--primary', "#e1684d");
+        document.querySelector(":root").style.setProperty('--secondary', "#e1684d35");
+        document.querySelector(":root").style.setProperty('--text', "#fff");
+        document.querySelector(":root").style.setProperty('--primDark', "#ae3e25");
+        document.querySelector("body").style.backgroundColor="var(--blackBg)";
+    }else{
+        document.querySelector(":root").style.setProperty('--primary', "#4071f4");
+        document.querySelector(":root").style.setProperty('--secondary', "#4071f435");
+        document.querySelector(":root").style.setProperty('--text', "#000");
+        document.querySelector(":root").style.setProperty('--primDark', "#3156bc");
+        document.querySelector("body").style.backgroundColor="var(--tertiary)";
     }
-
-    // document.querySelector(":root").style.setProperty('--blue', "#e1684d");
-    // document.querySelector(":root").style.setProperty('--lightBlue', "#e1684d35");
-    // document.querySelector("body").style.backgroundColor="var(--blackBg)";
 }
 
 let certiCont= document.querySelectorAll(".flip-container")
